5e107f916bee449f535763477ddcb3b82a9bf0cc
Nikindrik <68081589+Nikindrik@users.noreply.github.com>
2024-11-20 17:10:53 +0300

    v0.0.3.4 PAIN...

--- analyzer.py
+++ analyzer.py
@@ -30,6 +30,48 @@ end.
     '''
 ]
 
+code = '''
+program 
+var a, i, sum, k: %;
+	r: !;
+	flag: $;
+begin 
+	a := 5 + 5;  {Это комментарий}
+	if (a > 5)
+	begin
+		writeln 'a больше 5';
+	end
+	else
+	begin
+        writeln 'a меньше или равно 5';
+	end
+	sum := 0;
+	for i := 0 to a step 1
+	begin
+		writeln 'i = ', i;
+		sum := sum + i;
+		next;
+	end
+	writeln 'sum = ', sum;
+	k := 10
+	while (k != 0)
+	begin
+		writeln 'k = ', k;
+		k := k + 1;
+	end
+	writeln 'Введите true либо false';
+	readln flag;
+	if (flag)
+	begin
+		while 'flag имеет значение true'
+	end
+	else
+	begin
+		while 'flag имеет значение false'
+	end
+end.
+'''
+
 for elem in all_codes:
     # Лексический анализ
     lexer = LexicalAnalyzer()
