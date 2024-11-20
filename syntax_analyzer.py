146bbfa7d98878c79312bd1f456222be8109e061
Nick <nazernov@gmail.com>
2024-11-20 04:03:23 +0300

    v0.0.3.4 PAIN...

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -14,7 +14,6 @@ class SyntaxAnalyzer:
         raise SyntaxError(f"Expected {token_type} {value}, got {token}")
 
     def advance(self):
-        """Сдвигает позицию на следующий токен."""
         self.position += 1
 
     def parse_program(self):
@@ -48,22 +47,23 @@ class SyntaxAnalyzer:
         while True:
             token = self.current_token()
             if token[0] == 'KEYWORD' and token[1] == 'begin':
-                self.parse_compound_statement()  # Обрабатываем вложенные блоки
+                self.parse_compound_statement()
             elif token[0] == 'ID':
                 self.parse_assignment()
             elif token[0] == 'KEYWORD' and token[1] == 'readln':
                 self.parse_input_statement()
             elif token[0] == 'KEYWORD' and token[1] == 'writeln':
                 self.parse_output_statement()
+            elif token[0] == 'KEYWORD' and token[1] == 'if':
+                self.parse_if_statement()
             elif token[0] == 'KEYWORD' and token[1] == 'for':
                 self.parse_for_loop()
             elif token[0] == 'KEYWORD' and token[1] == 'end':
                 return
             elif token[0] == 'DELIMITER' and token[1] == ';':
-                # Переход к следующему оператору
-                self.advance()  # Сдвигаем токены, ожидаем следующий оператор
+                self.advance()
             else:
-                raise SyntaxError(f"Неожиданное начало оператора: {token}")
+                raise SyntaxError(f"Unexpected statement start: {token}")
 
     def parse_compound_statement(self):
         self.expect('KEYWORD', 'begin')
@@ -76,54 +76,67 @@ class SyntaxAnalyzer:
         self.parse_expression()
 
     def parse_input_statement(self):
-        self.expect('KEYWORD', 'readln')  # TODO: ATTENTION self.expect('DELIMITER', '(')
+        self.expect('KEYWORD', 'readln')
         self.expect('ID')
         while self.current_token() and self.current_token()[0] == 'DELIMITER' and self.current_token()[1] == ',':
             self.expect('DELIMITER', ',')
             self.expect('ID')
-        # TODO: ATTENTION self.expect('DELIMITER', '(')
 
     def parse_output_statement(self):
-        self.expect('KEYWORD', 'writeln')   # TODO: ATTENTION self.expect('DELIMITER', '(')
+        self.expect('KEYWORD', 'writeln')
         self.parse_expression()
         while self.current_token() and self.current_token()[0] == 'DELIMITER' and self.current_token()[1] == ',':
             self.expect('DELIMITER', ',')
             self.parse_expression()
-        # TODO: ATTENTION self.expect('DELIMITER', '(')
 
     def parse_for_loop(self):
         self.expect('KEYWORD', 'for')
-        self.parse_assignment()  # Обрабатываем присваивание в цикле
+        self.parse_assignment()
         self.expect('KEYWORD', 'to')
-        self.parse_expression()  # Обрабатываем условие "до"
+        self.parse_expression()
         if self.current_token() and self.current_token()[0] == 'KEYWORD' and self.current_token()[1] == 'step':
             self.expect('KEYWORD', 'step')
-            self.parse_expression()  # Обрабатываем шаг
-        self.expect('ID', 'next')  # TODO: ATTENTION do
-        self.parse_statements()  # Обрабатываем тело цикла (один или несколько операторов внутри цикла)
+            self.parse_expression()
+        self.expect('KEYWORD', 'begin')
+        self.parse_statements()
+        self.expect('KEYWORD', 'end')
 
     def parse_expression(self):
         self.parse_term()
         while self.current_token() and self.current_token()[0] in {'ADD_OP', 'REL_OP'}:
-            self.position += 1
+            self.advance()
             self.parse_term()
 
     def parse_term(self):
         self.parse_factor()
         while self.current_token() and self.current_token()[0] == 'MUL_OP':
-            self.position += 1
+            self.advance()
             self.parse_factor()
 
     def parse_factor(self):
         token = self.current_token()
         if token[0] in {'ID', 'NUMBER', 'STRING'}:
-            self.position += 1
+            self.advance()
         elif token[0] == 'DELIMITER' and token[1] == '(':
             self.expect('DELIMITER', '(')
             self.parse_expression()
             self.expect('DELIMITER', ')')
         elif token[0] == 'ADD_OP' and token[1] in {'+', '-'}:
-            self.position += 1
+            self.advance()
             self.parse_factor()
         else:
             raise SyntaxError(f"Unexpected factor: {token}")
+
+    def parse_if_statement(self):
+        self.expect('KEYWORD', 'if')
+        self.expect('DELIMITER', '(')
+        self.parse_expression()
+        self.expect('DELIMITER', ')')
+        self.expect('KEYWORD', 'begin')
+        self.parse_statements()
+        self.expect('KEYWORD', 'end')
+        if self.current_token() and self.current_token()[0] == 'KEYWORD' and self.current_token()[1] == 'else':
+            self.advance()
+            self.expect('KEYWORD', 'begin')
+            self.parse_statements()
+            self.expect('KEYWORD', 'end')
